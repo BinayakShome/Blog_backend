29b0dbfaad90c29717956aae2645c2b1af2c14b2
BinayakShome <binayakshome3@gmail.com>
2026-02-19 23:27:26 +0530

    feat: CategoryApi completed

--- src/main/java/com/binayak/blog_backend/payloads/CategoryDto.java
+++ src/main/java/com/binayak/blog_backend/payloads/CategoryDto.java
@@ -1,6 +1,5 @@
 package com.binayak.blog_backend.payloads;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
